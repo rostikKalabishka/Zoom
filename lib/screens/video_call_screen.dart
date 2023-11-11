4c2579af387efaf5d6788ff5b28d979317f3b993
rostikKalabishka <rostik31052002@gmail.com>
2023-11-11 16:25:39 +0200

    added change

---
+++ lib/screens/video_call_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoCallScreen extends StatefulWidget {
+  const VideoCallScreen({super.key});
+
+  @override
+  State<VideoCallScreen> createState() => _VideoCallScreenState();
+}
+
+class _VideoCallScreenState extends State<VideoCallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
