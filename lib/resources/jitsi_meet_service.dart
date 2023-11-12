262571e49bc62860d781a20c7c007469f6dab56d
rostikKalabishka <rostik31052002@gmail.com>
2023-11-12 12:02:27 +0200

    add JitsiMeetService

---
+++ lib/resources/jitsi_meet_service.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:jitsi_meet_wrapper/jitsi_meet_wrapper.dart';
+import 'package:zoom/resources/auth_service.dart';
+
+class JitsiMeetService {
+  final AuthService _authService = AuthService();
+
+  Future createMeeting({
+    required String roomName,
+    required bool isAudioMuted,
+    required bool isVideoMuted,
+    String username = '',
+  }) async {
+    String name;
+    if (username.isEmpty) {
+      name = _authService.user.displayName!;
+    } else {
+      name = username;
+    }
+    var options = JitsiMeetingOptions(
+        roomNameOrUrl: roomName,
+        userDisplayName: name,
+        userEmail: _authService.user.email,
+        userAvatarUrl: _authService.user.photoURL,
+        isAudioMuted: isAudioMuted,
+        isVideoMuted: isVideoMuted);
+
+    debugPrint("JitsiMeetingOptions: $options");
+    await JitsiMeetWrapper.joinMeeting(
+      options: options,
+      listener: JitsiMeetingListener(
+        onOpened: () => debugPrint("onOpened"),
+        onConferenceWillJoin: (url) {
+          debugPrint("onConferenceWillJoin: url: $url");
+        },
+        onConferenceJoined: (url) {
+          debugPrint("onConferenceJoined: url: $url");
+        },
+        onConferenceTerminated: (url, error) {
+          debugPrint("onConferenceTerminated: url: $url, error: $error");
+        },
+        onAudioMutedChanged: (isMuted) {
+          debugPrint("onAudioMutedChanged: isMuted: $isMuted");
+        },
+        onVideoMutedChanged: (isMuted) {
+          debugPrint("onVideoMutedChanged: isMuted: $isMuted");
+        },
+        onScreenShareToggled: (participantId, isSharing) {
+          debugPrint(
+            "onScreenShareToggled: participantId: $participantId, "
+            "isSharing: $isSharing",
+          );
+        },
+        onParticipantJoined: (email, name, role, participantId) {
+          debugPrint(
+            "onParticipantJoined: email: $email, name: $name, role: $role, "
+            "participantId: $participantId",
+          );
+        },
+        onParticipantLeft: (participantId) {
+          debugPrint("onParticipantLeft: participantId: $participantId");
+        },
+        onParticipantsInfoRetrieved: (participantsInfo, requestId) {
+          debugPrint(
+            "onParticipantsInfoRetrieved: participantsInfo: $participantsInfo, "
+            "requestId: $requestId",
+          );
+        },
+        onChatMessageReceived: (senderId, message, isPrivate) {
+          debugPrint(
+            "onChatMessageReceived: senderId: $senderId, message: $message, "
+            "isPrivate: $isPrivate",
+          );
+        },
+        onChatToggled: (isOpen) => debugPrint("onChatToggled: isOpen: $isOpen"),
+        onClosed: () => debugPrint("onClosed"),
+      ),
+    );
+  }
+}
